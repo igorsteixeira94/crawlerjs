cbccbe0152d00aacecbf88c91331c97acce6b76a
Igorsteixeira94 <igorsteixeira94@gmail.com>
2020-05-11 17:20:48 -0300

    Chore: Replace , for . in name product

--- src/Record.js
+++ src/Record.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = function save(id, name, price){
     fs.writeFile(
       path.resolve(__dirname,'..','tmp','./products.csv'),
-      `${id},${name},${price}\n`,
+      `${id},${name.toString().replace(",", ".")},${price}\n`,
       {enconding:'utf-8',flag: 'a'},
       (err)=> {if(err) throw err})
   }
